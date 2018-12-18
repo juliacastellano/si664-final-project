129a37b97b1843775de457cafff26284e248ca63
Julia Castellano <juliacastellano@Julias-MacBook-Pro.local>
2018-12-18 13:12:36 -0800

    Removed duplicate file

--- hospital_data_creation.sql
+++
@@ -1,8 +0,0 @@
-create database if not exists hospital_data;
-
-use hospital_data;
-
-SET FOREIGN_KEY_CHECKS=0;
-DROP TABLE IF EXISTS state, zip_code, county, city, hospital,
-  payment_measure, payment_category, hospital_payment, value, value_category, hospital_value;
-SET FOREIGN_KEY_CHECKS=1;
